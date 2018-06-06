26ef800baf389cc6fe60c581c82ca520dd9638a6
thetriplet3 <thetriplet3@gmail.com>
2018-06-06 14:34:29 +0530

    Middle-Tier -Added controllers and routes for Books -Minor bug fixed

--- PP/2017/express/app.js
+++ PP/2017/express/app.js
@@ -1,9 +1,18 @@
 'use strict'
 
 var express = require('express');
+var bodyParser = require('body-parser')
+var cors = require('cors');
 
-
+var routes = require('./routes')
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use('/api/', routes);
 
 
 app.listen(3000, 'localhost', (err) => {
--- PP/2017/express/routes.js
+++ PP/2017/express/routes.js
@@ -4,7 +4,9 @@ var express = require('express')
 var routes = express.Router();
 
 var AuthourRoutes = require('./Author/author.route');
+var BookRoutes = require('./Book/book.route');
 
-routes.use('/author/', AuthourRoutes);
+routes.use('/authors/', AuthourRoutes);
+routes.use('/books/', BookRoutes);
 
 module.exports = routes;
